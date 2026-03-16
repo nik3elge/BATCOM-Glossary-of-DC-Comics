2695b068081837b30d7f6cc748d2a12b0d79275a
Nik Zelge <117744296+nik3elge@users.noreply.github.com>
2026-03-16 03:47:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+async function loadTerms() {
 
+const categories = await fetch("data/categories.json")
+.then(r => r.json());
+
+const characters = await fetch("data/characters.json")
+.then(r => r.json());
+
+const terms = [
+...characters
+];
+
+renderTerms(terms);
+
+}
+
+function renderTerms(terms) {
+
+const table = document.getElementById("termsBody");
+
+terms.forEach(term => {
+
+const row = document.createElement("tr");
+
+row.innerHTML = `
+<td>${term.category}</td>
+<td>${term.en.join(", ")}</td>
+<td>${term.ru.join(", ")}</td>
+<td>${term.description}</td>
+<td>${term.see_also.join(", ")}</td>
+<td>${term.translator_note ? "ⓘ" : ""}</td>
+`;
+
+table.appendChild(row);
+
+});
+
+}
+
+loadTerms();
